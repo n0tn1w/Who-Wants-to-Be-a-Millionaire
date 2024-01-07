a7630c2ddaca9fdc3cf1844934b68187107dd2cc
M2Mdani <daniel.manchevski@m2mservices.com>
2024-01-07 15:56:41 +0200

    Changed Architecture

---
+++ GameApp/GameApp/Form1.cs
@@ -0,0 +1,10 @@
+namespace GameApp
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Millionaire/Millionaire/Models/Question.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Millionaire.Models
+{
+    public enum Difficulty
+    {
+        Easy,
+        Medium,
+        Hard
+    }
+    public class Question
+    {
+        public int ID { get; set; }
+        public string Description { get; set; }
+        public List<Anaswer> Answers { get; set; }
+        public string CallAFriendText { get; set; }
+        public Difficulty Difficulty { get; set; }
+    }
+
+    public class Answer 
+    {
+        public int ID { get; set; }
+
+        public string Option { get; set; }
+        public bool IsCorrect { get; set; }
+        public double VotePercentage { get; set; }
+        public bool IsShownInFiftyFifty { get; set; }
+    }
+}
